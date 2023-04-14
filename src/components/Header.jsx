db66bb7f80292c6be90e0288cb2a347debd0407a
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-04-14 09:46:24 -0300

    Box shado nos cards dos filmes, ícone da página de favoritos alterada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import { Link, useNavigate } from 'react-router-dom';
 import styles from '../styles/Header.module.css'
 import logo from '../img/logo.svg'
 import lupa from '../img/lupa.svg'
-import FavoriteIncator from './FavoriteIncator';
+import { MovieContext } from './FavoritesMovies'
+import {ReactComponent as Heart} from '../img/heart-header.svg'
 
 function Header() {
 
   const [searchKey, setSearchKey] = React.useState('');
   const navigate = useNavigate()
+  const {favorito} = React.useContext(MovieContext)
 
   const searchMovies = (event) =>{
     event.preventDefault()
@@ -20,7 +22,12 @@ function Header() {
   return (
     <nav className={styles.nav}>
         <Link to="/"><img src={logo} alt="Logo" className={styles.logo} /></Link>
-        <FavoriteIncator/>
+        <div className={styles.heartContainer}>
+          <Link to='/favoritos'>
+          <Heart/>
+          </Link>
+          <span className={styles.favCount}>{favorito.length}</span>
+        </div>
         <form onSubmit={searchMovies}>
             <input type="text" placeholder='Busque um filme'onChange={(e)=> setSearchKey(e.target.value)}  value={searchKey}/>
             <button type="submit" className={styles.lupa}>
