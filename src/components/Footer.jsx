26c1b3cd8297d8be33b756087beba58e31e2d26c
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-04-16 11:05:44 -0300

    coração do footer alterado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from '../styles/Footer.module.css';
 
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-        <p>Desenvolvido com 💜 por <a className={styles.link} href="https://github.com/jacks0nsilva" target='_blank'>Jackson</a></p>
+        <p>Desenvolvido com 🤍 por <a className={styles.link} href="https://github.com/jacks0nsilva" target='_blank'>Jackson</a></p>
     </footer>
   )
 }
